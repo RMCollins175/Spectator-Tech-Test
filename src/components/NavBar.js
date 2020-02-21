4e926f0ce22c12fea60fb2bc038b08a10f2e8a60
Rory Collins <rorytothecollins@googlemail.com>
2020-02-21 13:47:29 +0000

    edit NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -23,7 +23,7 @@ export default class NavBar extends Component {
       <div>
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
           <a className="navbar-brand" href="/">
-            News
+            Home
           </a>
           <button
             className="navbar-toggler"
@@ -38,11 +38,6 @@ export default class NavBar extends Component {
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mr-auto">
-              <li className="nav-item active">
-                <a className="nav-link" href="/">
-                  Home <span className="sr-only">(current)</span>
-                </a>
-              </li>
               <li className="nav-item dropdown">
                 <a
                   className="nav-link dropdown-toggle"
